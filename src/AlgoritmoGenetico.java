249acc39ef5f8bfc0762474c4999017f4aff4d11
abranllis <47560487+abranllis@users.noreply.github.com>
2023-04-27 12:36:19 +0200

    1

--- src/AlgoritmoGenetico.java
+++ src/AlgoritmoGenetico.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 
 public class AlgoritmoGenetico {
@@ -5,10 +7,9 @@ public class AlgoritmoGenetico {
     private static final int ELEMENTOS_POBLACION = 10;
     private static final int MAX_ITERACIONES = 1000;
     private static final int NUM_VARIACION_GENES = 10;
-
     private static final int[] CODIGO_OBJETIVO = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    // Función de cruzamiento
+    // Función que cruza 2 padres y devuelve el hijo
     public static int[] cruzar(int[] padre1, int[] padre2) {
         Random random = new Random();
         int puntoCorte = random.nextInt(padre1.length); // Seleccionar un punto de corte aleatorio
@@ -33,11 +34,29 @@ public class AlgoritmoGenetico {
             return fitness;
     }
 
+    public static int[][] ordenarPoblacion(int[][] poblacion) {
+        Comparator<int[]> comparator = new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[10], b[10]);
+            }
+        };
 
-    // Ejemplo de uso
+        // Ordenar el array utilizando el comparador personalizado
+        Arrays.sort(poblacion, comparator);
+        return poblacion;
+    }
+
+    public static void mostrarPoblacion(int[][] poblacion){
+        for ( int i = 0; i < ELEMENTOS_POBLACION; i++) {
+            for (int j = 0; j < LONGITUD_CROMOSOMA; j++) {
+                System.out.print(poblacion[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        // FILAS = ELEMENTOS_POBLACION
-        //COLUMNAS = LONGITUD_CROMOSOMA
 
         int[][] poblacion = new int[ELEMENTOS_POBLACION][LONGITUD_CROMOSOMA];
 
@@ -49,34 +68,22 @@ public class AlgoritmoGenetico {
             }
         }
 
-        //Mostrar por pantalla los valores de la poblacion inicial
-        System.out.println("POBLACION INICIAL : ");
-        for ( int i = 0; i < ELEMENTOS_POBLACION; i++) {
-            for (int j = 0; j < LONGITUD_CROMOSOMA; j++) {
-                System.out.print(poblacion[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        // Definir la matriz para almacenar los resultados del fitness
-        //int[] VectorConFitness = new int[ELEMENTOS_POBLACION];
-
         // Calcular el fitness para cada cromosoma
         for (int i = 0; i < ELEMENTOS_POBLACION  ; i++) {
              //   VectorConFitness[i] = calcularFitness(poblacion[i]);
                  poblacion[i][LONGITUD_CROMOSOMA-1] = calcularFitness(poblacion[i]);
         }
 
-        //Mostrar por pantalla la matriz de FITNESS
-       /* System.out.println("VECTOR DE FITNESS: ");
-        for ( int i = 0; i < ELEMENTOS_POBLACION; i++) {
-                System.out.println(VectorConFitness[i]);
-        }
-*/
+        //Mostrar por pantalla los valores de la poblacion inicial + fitness
+        System.out.println("POBLACION 1 : ");
+        mostrarPoblacion(poblacion);
 
-        //ORDENAMOS LA POBLACION SEGUN SU FITNESS
-        //CADA CROMOSOMA TIENE UN FITNESS Y LOS ORDENAREMOS DE MENOR FITNESS A MAYOR
-        //FITNESS = NUMERO DE GENES DIFERENTES DEL OBJETIVO
+        // Ordenar el array utilizando el comparador personalizado
+        poblacion = ordenarPoblacion(poblacion);
+
+        //Mostrar POBLACION ORDENADA POR FITNESS
+        System.out.println("POBLACION ORDENADA : ");
+        mostrarPoblacion(poblacion);
 
 
 
